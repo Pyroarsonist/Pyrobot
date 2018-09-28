b5a3586159859561017b46d056f0bf50ff4ace4f
Pyroarsonist <velikiydan@gmail.com>
2018-09-28 22:54:49 +0300

    added plot rofl

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,4 +4,7 @@ import regexes from './regexes';
 const botId = 503111149;
 const pyroarsonistId = 358208027;
 
-export { answers, regexes, botId, pyroarsonistId };
+const plotUrl =
+  'https://sun9-2.userapi.com/c830208/v830208240/ebe18/5gLJB1INRCo.jpg';
+
+export { answers, regexes, botId, pyroarsonistId, plotUrl };
---
+++ src/data/triggers/plot.js
@@ -0,0 +1,1 @@
+export default text => !!text.match(/график|plot/gi);
