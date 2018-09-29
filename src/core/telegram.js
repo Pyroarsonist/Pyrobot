3a27188856ad671504d640bc6ee7847e2f0d9d7f
Pyroarsonist <velikiydan@gmail.com>
2018-09-29 15:57:03 +0300

    refactored to telegraf

--- src/core/telegram.js
+++ src/core/telegram.js
@@ -1,34 +1,41 @@
-import TelegramBot from 'node-telegram-bot-api';
+import Telegraf from 'telegraf';
 import logger from './logger';
 import commamds from '../data/commands';
 import { server, bot as botConfig, sslFolder } from '../config';
 
-const options = {
-  webHook: {
-    port: server.port,
-    key: `${sslFolder}/key.pem`, // Path to file with PEM private key
-    cert: `${sslFolder}/server.crt`, // Path to file with PEM certificate
-  },
-};
+// const options = {
+//   webHook: {
+//     port: server.port,
+//     key: `${sslFolder}/key.pem`, // Path to file with PEM private key
+//     cert: `${sslFolder}/server.crt`, // Path to file with PEM certificate
+//   },
+// };
 
 // eslint-disable-next-line import/no-mutable-exports
 let bot = null;
 export default () =>
   new Promise((res, rej) => {
+    if (!botConfig.token) {
+      throw new Error('No telegram bot key supplied');
+    }
     try {
       console.info('Initializing telegram bot');
+      // todo: add webhooks
       if (server.publicUrl && server.publicPort && sslFolder) {
         // telegram needs a ssl for webhook
-        bot = new TelegramBot(botConfig.token, options);
-        bot.setWebHook(
-          `${server.publicUrl}:${server.publicPort}/bot${botConfig.token}`,
-        );
+        // bot = new Telegraf(botConfig.token, options);
+        // bot.setWebHook(
+        //   `${server.publicUrl}:${server.publicPort}/bot${botConfig.token}`,
+        // );
       } else {
-        bot = new TelegramBot(botConfig.token, { polling: true });
+        bot = new Telegraf(botConfig.token);
       }
 
       // loading commands
       commamds();
+
+      // starting polling
+      bot.startPolling();
     } catch (e) {
       logger.error(e);
       rej(e);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import 'babel-polyfill';
+import 'isomorphic-fetch';
 import express from 'express';
 import { createServer } from 'http';
-import 'isomorphic-fetch';
 import bodyParser from 'body-parser';
 
 import logger from './core/logger';
 import telegram from './core/telegram';
-import { server as serverConfig, bot } from './config';
+import { server as serverConfig } from './config';
 
 process
   .on('unhandledRejection', (reason, p) => {
@@ -18,21 +18,12 @@ process
     process.exit(1);
   });
 
-if (!bot || !bot.token) {
-  console.error('No telegram bot key supplied');
-}
-
 const app = express();
 const server = createServer(app);
 
 const promise = telegram().catch(err => console.error(err.stack));
 
 app.use(bodyParser.json());
-
-app.post(`/bot${bot.token}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
 
 promise.then(() => {
   // init server
