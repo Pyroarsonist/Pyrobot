6383b7db81f659cfbdcdab43b377f8e01d89da48
Pyroarsonist <velikiydan@gmail.com>
2019-05-06 11:55:09 +0300

    added hours capturing in regex of msg

--- src/data/triggers/getLastUniqueMessagesTrigger.js
+++ src/data/triggers/getLastUniqueMessagesTrigger.js
@@ -1,7 +1,8 @@
 import { Chat, Message } from 'data/models';
 import Bluebird from 'bluebird';
+import moment from 'moment';
 
-const regex = /сообщения|messages|msg/gi;
+const regex = /сообщения|messages|msg(?<hours> \d*)/gi;
 
 export default async ctx => {
   if (!ctx.pyroInfo.isAdmin) return false;
@@ -9,33 +10,44 @@ export default async ctx => {
   const response = !!ctx.message.text.match(regex);
   if (response) {
     try {
-      const date7DaysAgo = Date.now() - 7 * 24 * 60 * 60 * 1000;
-      const chats = await Chat.find({
-        updatedAt: { $gt: date7DaysAgo },
-      });
+      const hours = Number.parseInt(regex.exec(ctx.message.text).groups.hours);
+      if (Number.isNaN(hours)) {
+        await ctx.reply(
+          `Кол-во часов неправильное значение:\n${hours}`,
+          ctx.pyroInfo.replyOptions,
+        );
+      } else {
+        const chats = await Chat.find({
+          updatedAt: {
+            $gte: moment()
+              .subtract({ hours })
+              .toISOString(),
+          },
+        });
 
-      await Bluebird.each(
-        chats,
-        async chat => {
-          const relatedMessages = await Message.find({
-            // eslint-disable-next-line
-            chat: chat._id,
-          })
-            .sort({ date: -1 })
-            .limit(10)
-            .populate('user');
-          const message = `${JSON.stringify(
-            chat.formatted,
-            null,
-            2,
-          )}\n${relatedMessages
-            .reverse()
-            .map(x => JSON.stringify(x.formatted, null, 2))
-            .join('\n')}`;
-          await ctx.reply(message, ctx.pyroInfo.replyOptions);
-        },
-        { concurrency: 1 },
-      );
+        await Bluebird.each(
+          chats,
+          async chat => {
+            const relatedMessages = await Message.find({
+              // eslint-disable-next-line
+              chat: chat._id,
+            })
+              .sort({ date: -1 })
+              .limit(10)
+              .populate('user');
+            const message = `${JSON.stringify(
+              chat.formatted,
+              null,
+              2,
+            )}\n${relatedMessages
+              .reverse()
+              .map(x => JSON.stringify(x.formatted, null, 2))
+              .join('\n')}`;
+            await ctx.reply(message, ctx.pyroInfo.replyOptions);
+          },
+          { concurrency: 1 },
+        );
+      }
     } catch (e) {
       console.error(e);
       await ctx.reply('крит, ныа', ctx.pyroInfo.replyOptions);
